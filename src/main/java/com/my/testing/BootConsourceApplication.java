9a66ca2a756789142efce776c71035701ce6cd7f
Ravi Palakodeti <ravi.palakodeti@ge.com>
2016-08-15 08:25:30 -0500

    initial commit

---
+++ src/main/java/com/my/testing/BootConsourceApplication.java
@@ -0,0 +1,12 @@
+package com.my.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootConsourceApplication.class, args);
+	}
+}
