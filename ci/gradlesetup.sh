bf2651c88c5870b88449bb7de8f98cf9f97d33cb
Ravi Palakodeti <ravi.palakodeti@ge.com>
2016-08-16 02:06:46 -0500

    ci

--- ci/gradlesetup.sh
+++ ci/gradlesetup.sh
@@ -10,6 +10,10 @@ ls /
 
 cat /etc/passwd
 
+sudo useradd -m -s /bin/bash -U gradle
+
+su gradle
+
 ls /home
 
 touch .gradle/gradle.properties && echo 'cfUsername={{cf-username}} \ncfPassword={{cf-password}}' >> .gradle/gradle.properties
