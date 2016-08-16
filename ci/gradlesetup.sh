5b0f40a6ee410e9631aec4b1f9d03cefba9d1209
Ravi Palakodeti <ravi.palakodeti@ge.com>
2016-08-16 02:38:12 -0500

    ci

--- ci/gradlesetup.sh
+++ ci/gradlesetup.sh
@@ -18,6 +18,6 @@ su - gradle -c 'mkdir .gradle'
 
 su - gradle -c 'touch ~/.gradle/gradle.properties && echo "cfUsername={{cf-username}} \ncfPassword={{cf-password}}" >> ~/.gradle/gradle.properties'
 
-echo "file created"
+echo "file created {{cf-username}} "
 
-su - gradle -c 'less ~/.gradle/gradle.properties'
+#su - gradle -c 'less ~/.gradle/gradle.properties'
